1742242ba99c1bda193a7a3b6f262eb9fa858ea4
Xhistr <vaibhavdwivedi.iitr@gmail.com>
2022-07-10 21:09:23 +0530

    Small Changes - Change cursor type - Format Code

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ var mode = DEFAULT_MODE;
 var color = DEFAULT_COLOR;
 var size = DEFAULT_SIZE;
 
-const colorpicker=document.querySelector('#colorselector');
-const sizeSelector=document.querySelector('.slider');
-const canvas=document.querySelector('#canvas');
+const colorpicker = document.querySelector('#colorselector');
+const sizeSelector = document.querySelector('.slider');
+const canvas = document.querySelector('#canvas');
 
 const colormode = document.querySelector('#color');
-const rainbowmode= document.querySelector('#rainbow');
-const erasermode= document.querySelector('#eraser');
+const rainbowmode = document.querySelector('#rainbow');
+const erasermode = document.querySelector('#eraser');
 const clearmode = document.querySelector('#clear');
 
 colormode.onclick = selectedMode;
@@ -25,36 +25,36 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
-function makeElement(){
+function makeElement() {
     element = document.createElement('div');
-    element.setAttribute('style',`width:${500/size}px;height:${500/size}px;background-color:#fefefe;`);
+    element.setAttribute('style', `width:${500 / size}px;height:${500 / size}px;background-color:#fefefe;`);
     canvas.appendChild(element);
-    element.addEventListener('mouseover',(e)=>fillColor(e));
-    element.addEventListener('mousedown',(e)=>fillColor(e));
+    element.addEventListener('mouseover', (e) => fillColor(e));
+    element.addEventListener('mousedown', (e) => fillColor(e));
 }
 
-function makeGrid(){
-    elements_count = size*size;
-    canvas.innerHTML="";
-    for(let i=0;i<elements_count;i++){
+function makeGrid() {
+    elements_count = size * size;
+    canvas.innerHTML = "";
+    for (let i = 0; i < elements_count; i++) {
         makeElement();
     }
 }
-function fillColor(e){
+function fillColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return;
-    if(mode=="color"){
-        color=colorpicker.value;
+    if (mode == "color") {
+        color = colorpicker.value;
     }
-    if(mode=="eraser"){
+    if (mode == "eraser") {
         color = "#ffffff";
     }
-    if(mode=="rainbow"){
-        color=getRandomColor();
+    if (mode == "rainbow") {
+        color = getRandomColor();
     }
     e.target.style.background = color;
 }
 
-function selectedMode(){
+function selectedMode() {
     mode = this.id;
     console.log(mode);
     btns = document.querySelectorAll('.btn');
@@ -68,17 +68,17 @@ function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+}
 
- function sizeSelection() {
+function sizeSelection() {
     size = this.value;
-    document.querySelector('#sizedisplay').textContent=size+" X "+size;
+    document.querySelector('#sizedisplay').textContent = size + " X " + size;
     makeGrid();
-  }
-  
+}
+
 
 makeGrid();
 
