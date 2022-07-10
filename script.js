8b8c83b715bb2373532bb485c2911d8cd336b07e
Xhistr <vaibhavdwivedi.iitr@gmail.com>
2022-07-10 15:42:49 +0530

    Complete UI Part Of App - Made Header and Footer. - Made Canvas for Sketching. - Made Setting Bar. - Styled every component.

---
+++ script.js
@@ -0,0 +1,6 @@
+for(var i=0;i<2500;i++){
+    canvas = document.querySelector('#canvas');
+    element = document.createElement('div');
+    element.setAttribute('style','width:10px;height:10px;background-color:#fefefe;');
+    canvas.appendChild(element);
+}
